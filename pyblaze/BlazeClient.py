09251df3858c2a1ac332b4d00d803c57e3a4c5ae
Álvaro Paiva <alvarofepipa@gmail.com>
2022-06-18 15:31:01 -0300

    refactor: BlazeClient base response treatment

--- pyblaze/BlazeClient.py
+++ pyblaze/BlazeClient.py
@@ -19,6 +19,12 @@ class BlazeClient:
         if version == 2:
             self._url_api = self.URL_API_V2
 
+    def basic_response_treatment(self, response):
+        if response.status_code == 200:
+            return response.json()
+
+        return response
+
     def _send_request(self, method, url, **kwargs):
         kwargs['headers'] = {
             # 'User-Agent': self._user_agent.random,
@@ -26,73 +32,68 @@ class BlazeClient:
                           'AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/41.0.2228.0 Safari/537.36',
         }
-        response = self._session.request(method, url, **kwargs)
-
-        if response.status_code == 200:
-            return response.json()
-
-        return response
+        return self._session.request(method, url, **kwargs)
 
     def announcement(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/announcement',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def chat_rooms(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/chat_rooms',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def chat_room(self, chat_number: int = 2):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/chat_rooms/{chat_number}',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def country(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/country',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def currencies(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/currencies',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def version(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/version',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def settings(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/settings',
         )
 
-        return data
+        return self.basic_response_treatment(response)
 
     def time(self):
-        data = self._send_request(
+        response = self._send_request(
             'GET',
             f'{self._url_api}/time',
         )
 
-        return data
+        return self.basic_response_treatment(response)
