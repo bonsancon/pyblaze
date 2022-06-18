e4e3fb6b538a052171877517c804a9c6426304c4
Álvaro Paiva <alvarofepipa@gmail.com>
2022-06-18 13:37:47 -0300

    chore: Set 0.0.1 version

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bonsancon-pyblaze',
+    version='0.0.1',
+    author='Álvaro Ferreira Pires de Paiva',
+    author_email='alvarofepipa@gmail.com',
+    description='Blaze API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bonsancon/pyblaze',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
